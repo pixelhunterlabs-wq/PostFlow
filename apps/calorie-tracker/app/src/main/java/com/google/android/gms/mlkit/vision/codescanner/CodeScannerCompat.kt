1bb0ee697b6fb729323cfe43f8033acbb082cb16
GameforgeStudio <pixelhunterlabs@gmail.com>
2026-09-06 22:38:44 +0300

    Wrap Google Code Scanner options compatibility API

--- apps/calorie-tracker/app/src/main/java/com/google/android/gms/mlkit/vision/codescanner/CodeScannerCompat.kt
+++ apps/calorie-tracker/app/src/main/java/com/google/android/gms/mlkit/vision/codescanner/CodeScannerCompat.kt
@@ -1,4 +1,29 @@
 package com.google.android.gms.mlkit.vision.codescanner
 
-typealias GmsBarcodeScannerOptions = com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
-typealias GmsBarcodeScanning = com.google.mlkit.vision.codescanner.GmsBarcodeScanning
+import android.content.Context
+
+class GmsBarcodeScannerOptions private constructor(
+    internal val delegate: com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+) {
+    class Builder {
+        private val delegate = com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions.Builder()
+
+        fun setBarcodeFormats(format: Int, vararg moreFormats: Int): Builder = apply {
+            delegate.setBarcodeFormats(format, *moreFormats)
+        }
+
+        fun enableAutoZoom(): Builder = apply {
+            delegate.enableAutoZoom()
+        }
+
+        fun build(): GmsBarcodeScannerOptions = GmsBarcodeScannerOptions(delegate.build())
+    }
+}
+
+object GmsBarcodeScanning {
+    fun getClient(
+        context: Context,
+        options: GmsBarcodeScannerOptions
+    ): com.google.mlkit.vision.codescanner.GmsBarcodeScanner =
+        com.google.mlkit.vision.codescanner.GmsBarcodeScanning.getClient(context, options.delegate)
+}
