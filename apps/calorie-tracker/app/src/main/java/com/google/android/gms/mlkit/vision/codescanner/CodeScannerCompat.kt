d89c7260ecd73e656b0c8378aa2ff243d7421208
GameforgeStudio <pixelhunterlabs@gmail.com>
2026-09-06 22:31:56 +0300

    Fix Google Code Scanner package compatibility

---
+++ apps/calorie-tracker/app/src/main/java/com/google/android/gms/mlkit/vision/codescanner/CodeScannerCompat.kt
@@ -0,0 +1,4 @@
+package com.google.android.gms.mlkit.vision.codescanner
+
+typealias GmsBarcodeScannerOptions = com.google.android.gms.codescanner.GmsBarcodeScannerOptions
+typealias GmsBarcodeScanning = com.google.android.gms.codescanner.GmsBarcodeScanning
