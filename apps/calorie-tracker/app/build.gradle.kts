8881bfe8feade4ea9407a704821036b5850849ea
GameforgeStudio <pixelhunterlabs@gmail.com>
2026-09-06 21:51:53 +0300

    Remove obsolete Kotlin Android plugin with AGP 9

--- apps/calorie-tracker/app/build.gradle.kts
+++ apps/calorie-tracker/app/build.gradle.kts
@@ -2,7 +2,6 @@ import java.util.Properties
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
 }
